4f8b7fb47f9db0190f1a99582631174b397fabf1
MANDLA HEMANTH <25eg107f44@anurag.edu.in>
2025-12-25 17:51:15 +0530

    Update app.py

--- carecloud/app.py
+++ carecloud/app.py
@@ -272,5 +272,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
