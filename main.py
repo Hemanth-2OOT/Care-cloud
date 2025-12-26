63551c73f4285bf024903fe3990a4ab78e832a7b
MANDLA HEMANTH <25eg107f44@anurag.edu.in>
2025-12-26 13:10:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 from carecloud.app import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # Railway will inject a PORT, but if not, it defaults to 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+    
