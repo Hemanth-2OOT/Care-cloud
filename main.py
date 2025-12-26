ee49165e00a1dacf23734ebd3033765bc500ff9d
MANDLA HEMANTH <25eg107f44@anurag.edu.in>
2025-12-26 13:34:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
 from carecloud.app import app
 
-if __name__ == "__main__":
-    # This allows Railway to tell the app which port to use
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
-    
+# Do not include the if __name__ == "__main__": app.run() block 
+# when deploying to Railway with Gunicorn. 
+# Gunicorn will automatically bind to the correct $PORT variable.
